49134eb8c87cf9bc510c92afaa5d65fd67d9a93e
ghadgemadhuri <ghadgemadhuri86@gmail.com>
2020-07-20 23:52:18 +0530

    initial commit

---
+++ mygraph/js/script.js
@@ -0,0 +1,117 @@
+var myCanvas = document.getElementById("myCanvas");
+myCanvas.width = 1200;
+myCanvas.height = 500;
+var ctx = myCanvas.getContext("2d");
+
+function drawLine(ctx, startX, startY, endX, endY, color) {
+    ctx.save();
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(endX, endY);
+    ctx.stroke();
+    ctx.restore();
+}
+
+function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height, color) {
+    ctx.save();
+    ctx.fillStyle = color;
+    ctx.fillRect(upperLeftCornerX, upperLeftCornerY, width, height);
+    ctx.restore();
+}
+
+var Barchart = function (options) {
+    this.options = options;
+    this.canvas = options.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.colors = options.colors;
+    console.log('class', options);
+    this.draw = function () {
+        var maxValue = 0;
+        for (var categ in this.options.data) {
+            maxValue = Math.max(maxValue, this.options.data[categ]);
+        }
+        var canvasActualHeight = this.canvas.height - this.options.padding * 2;
+        var canvasActualWidth = this.canvas.width - this.options.padding * 2;
+
+        //drawing the grid lines
+        var gridValue = 0;
+        while (gridValue <= maxValue) {
+            var gridY = canvasActualHeight * (1 - gridValue / maxValue) + this.options.padding;
+            drawLine(
+                this.ctx,
+                0,
+                gridY,
+                this.canvas.width,
+                gridY,
+                this.options.gridColor
+            );
+
+            //writing grid markers
+            this.ctx.save();
+            this.ctx.fillStyle = this.options.gridColor;
+            this.ctx.textBaseline = "bottom";
+            this.ctx.font = "bold 10px Arial";
+            this.ctx.fillText(gridValue, 10, gridY - 2);
+            this.ctx.restore();
+
+            gridValue += this.options.gridScale;
+        }
+
+        //drawing the bars
+        var barIndex = 0;
+        var numberOfBars = Object.keys(this.options.data).length;
+        var barSize = (canvasActualWidth) / numberOfBars;
+
+        for (categ in this.options.data) {
+            var val = this.options.data[categ];
+            var barHeight = Math.round(canvasActualHeight * val / maxValue);
+            drawBar(
+                this.ctx,
+                this.options.padding + barIndex * barSize,
+                this.canvas.height - barHeight - this.options.padding,
+                barSize,
+                barHeight,
+                this.colors[barIndex % this.colors.length]
+            );
+
+            barIndex++;
+        }
+
+        //drawing series name
+        this.ctx.save();
+        this.ctx.textBaseline = "bottom";
+        this.ctx.textAlign = "center";
+        this.ctx.fillStyle = "#000000";
+        this.ctx.font = "bold 14px Arial";
+        this.ctx.fillText(this.options.seriesName, this.canvas.width / 2, this.canvas.height);
+        this.ctx.restore();
+
+        //draw legend
+        barIndex = 0;
+        var legend = document.querySelector("legend[for='myCanvas']");
+        var ul = document.createElement("ul");
+        ul.style.display = "inline";
+        legend.append(ul);
+        for (categ in this.options.data) {
+            var li = document.createElement("li");
+            li.style.listStyle = "none";
+            li.style.borderLeft = "20px solid " + this.colors[barIndex % this.colors.length];
+            li.style.padding = "5px";
+            li.textContent = categ;
+            ul.append(li);
+            barIndex++;
+        }
+    }
+}
+
+async function getData() {
+    let url = 'https://api.exchangeratesapi.io/history?start_at=2019-01-01&end_at=2019-12-31&base=EUR&symbols=INR';
+    const resp = await fetch(url)
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function (data) {
+          	return createData(data.rates);
+        })
+}
+
+getData();
